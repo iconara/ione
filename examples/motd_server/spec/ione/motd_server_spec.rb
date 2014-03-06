ee31c7c561a62889c5d39a086adb42bb67d8ea3f
Theo <theo@iconara.net>
2014-03-06 21:57:16 +0100

    Add a server example

---
+++ examples/motd_server/spec/ione/motd_server_spec.rb
@@ -0,0 +1,39 @@
+# encoding: utf-8
+
+require 'ione/motd_server'
+
+
+module Ione
+  describe MotdServer do
+    let :server do
+      described_class.new(port, motd_path)
+    end
+
+    let :port do
+      rand(2**15) + 2**15
+    end
+
+    let :motd_path do
+      f = Tempfile.new('motd')
+      f.puts('Lorem ipsum dolor sit')
+      f.close
+      f.path
+    end
+
+    context 'when accepting a connection' do
+      before do
+        server.start.value
+      end
+
+      after do
+        server.stop.value
+      end
+
+      it 'responds with the contents of the motd file and closes the connection' do
+        socket = TCPSocket.new('127.0.0.1', port)
+        result = socket.read
+        result.should == "Lorem ipsum dolor sit\n"
+      end
+    end
+  end
+end
