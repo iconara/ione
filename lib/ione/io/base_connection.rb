3115426cf6fd2932bd71d1081b261fb2fd1fdaea
Theo <theo@iconara.net>
2014-09-15 08:16:28 +0200

    Make sure that *Connection#close isn’t run concurrently
    This closes #8. Thanks to @mimperatore for finding the issue and providing a patch.

--- lib/ione/io/base_connection.rb
+++ lib/ione/io/base_connection.rb
@@ -19,7 +19,10 @@ module Ione
       #
       # @return [true, false] returns false if the connection was already closed
       def close(cause=nil)
-        return false if @state == :closed
+        @lock.synchronize do
+          return false if @state == :closed
+          @state = :closed
+        end
         if @io
           begin
             @io.close
@@ -28,7 +31,6 @@ module Ione
             # nothing to do, the socket was most likely already closed
           end
         end
-        @state = :closed
         if cause && !cause.is_a?(IoError)
           cause = ConnectionClosedError.new(cause.message)
         end
