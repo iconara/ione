dc729144786e24468cd2f1e45d56374a960f6ea5
Theo <theo@iconara.net>
2014-11-08 17:27:35 +0100

    Rewrite the Redis client example with streams

--- examples/redis_client/lib/ione/redis_client.rb
+++ examples/redis_client/lib/ione/redis_client.rb
@@ -13,60 +13,48 @@ module Ione
       @host = host
       @port = port
       @reactor = Ione::Io::IoReactor.new
+      @responses = []
     end
 
     def connect
       f = @reactor.start
       f = f.flat_map { @reactor.connect(@host, @port) }
-      f = f.map { |connection| RedisProtocolHandler.new(connection) }
-      f.on_value { |protocol_handler| @protocol_handler = protocol_handler }
+      f.on_value do |connection|
+        @connection = connection
+        process_responses(connection.to_stream)
+      end
       f.map(self)
     end
 
-    def method_missing(name, *args)
-      @protocol_handler.send_request(name, *args)
-    end
-  end
-
-  class LineProtocolHandler
-    def initialize(connection)
-      @connection = connection
-      @connection.on_data(&method(:process_data))
-      @buffer = Ione::ByteBuffer.new
-      @requests = []
-    end
-
-    def on_line(&listener)
-      @line_listener = listener
-    end
-
-    def write(command_string)
-      @connection.write(command_string)
-    end
-
-    def process_data(new_data)
-      lines = []
-      @buffer << new_data
-      while (newline_index = @buffer.index("\r\n"))
-        line = @buffer.read(newline_index + 2)
-        line.chomp!
-        lines << line
+    def process_responses(byte_stream)
+      line_stream = byte_stream.aggregate(Ione::ByteBuffer.new) do |data, downstream, buffer|
+        buffer << data
+        while (newline_index = buffer.index("\r\n"))
+          line = buffer.read(newline_index + 2)
+          line.chomp!
+          downstream << line
+        end
+        buffer
       end
-      lines.each do |line|
-        @line_listener.call(line) if @line_listener
+      response_stream = line_stream.aggregate(RedisProtocol::BaseState.new) do |line, downstream, state|
+        state = state.feed_line(line)
+        if state.response?
+          downstream << [state.response, state.error?]
+        end
+        state
       end
+      response_stream.each do |response, error|
+        promise = @responses.shift
+        if error
+          promise.fail(StandardError.new(response))
+        else
+          promise.fulfill(response)
+        end
+      end
+      self
     end
-  end
 
-  class RedisProtocolHandler
-    def initialize(connection)
-      @line_protocol = LineProtocolHandler.new(connection)
-      @line_protocol.on_line(&method(:handle_line))
-      @responses = []
-      @state = BaseState.new(method(:handle_response))
-    end
-
-    def send_request(*args)
+    def method_missing(*args)
       promise = Ione::Promise.new
       @responses << promise
       request = "*#{args.size}\r\n"
@@ -74,87 +62,97 @@ module Ione
         arg_str = arg.to_s
         request << "$#{arg_str.bytesize}\r\n#{arg_str}\r\n"
       end
-      @line_protocol.write(request)
+      @connection.write(request)
       promise.future
     end
 
-    def handle_response(result, error=false)
-      promise = @responses.shift
-      if error
-        promise.fail(StandardError.new(result))
-      else
-        promise.fulfill(result)
-      end
-    end
+    module RedisProtocol
+      class State
+        attr_reader :next_state
 
-    def handle_line(line)
-      @state = @state.handle_line(line)
-    end
-
-    class State
-      def initialize(result_handler)
-        @result_handler = result_handler
-      end
-
-      def complete(result)
-        @result_handler.call(result)
-      end
-
-      def fail(message)
-        @result_handler.call(message, true)
-      end
-    end
-
-    class BulkState < State
-      def handle_line(line)
-        complete(line)
-        BaseState.new(@result_handler)
-      end
-    end
-
-    class MultiBulkState < State
-      def initialize(result_handler, expected_elements)
-        super(result_handler)
-        @expected_elements = expected_elements
-        @elements = []
-      end
-
-      def handle_line(line)
-        if line.start_with?('$')
-          line.slice!(0, 1)
-          if line.to_i == -1
-            @elements << nil
-          end
-        else
-          @elements << line
+        def initialize
+          @next_state = self
         end
-        if @elements.size == @expected_elements
-          complete(@elements)
-          BaseState.new(@result_handler)
-        else
-          self
+
+        def response?
+          false
+        end
+
+        def continue(next_state=self)
+          next_state
+        end
+
+        def complete(response, error=false)
+          CompleteState.new(response, error)
         end
       end
-    end
 
-    class BaseState < State
-      def handle_line(line)
-        next_state = self
-        first_char = line.slice!(0, 1)
-        case first_char
-        when '+' then complete(line)
-        when ':' then complete(line.to_i)
-        when '-' then fail(line)
-        when '$'
-          if line.to_i == -1
-            complete(nil)
+      class BulkState < State
+        def feed_line(line)
+          complete(line)
+        end
+      end
+
+      class MultiBulkState < State
+        def initialize(expected_elements)
+          super()
+          @expected_elements = expected_elements
+          @elements = []
+        end
+
+        def feed_line(line)
+          if line.start_with?('$')
+            line.slice!(0, 1)
+            if line.to_i == -1
+              @elements << nil
+            end
           else
-            next_state = BulkState.new(@result_handler)
+            @elements << line
           end
-        when '*'
-          next_state = MultiBulkState.new(@result_handler, line.to_i)
+          if @elements.size == @expected_elements
+            complete(@elements)
+          else
+            continue
+          end
         end
-        next_state
+      end
+
+      class BaseState < State
+        def feed_line(line)
+          first_char = line.slice!(0, 1)
+          case first_char
+          when '+' then complete(line)
+          when ':' then complete(line.to_i)
+          when '-' then complete(line, true)
+          when '$'
+            if line.to_i == -1
+              complete(nil)
+            else
+              continue(BulkState.new)
+            end
+          when '*'
+            continue(MultiBulkState.new(line.to_i))
+          else
+            continue
+          end
+        end
+      end
+
+      class CompleteState < BaseState
+        attr_reader :response
+
+        def initialize(response, error=false)
+          @response = response
+          @error = error
+        end
+
+        def response?
+          true
+        end
+
+        def error?
+          @error
+        end
       end
     end
   end
